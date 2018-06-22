331f4c7189199544f2c72ffc2ecaf1648f64f761
yvan674 <y_satyawan@hotmail.com>
2018-06-22 11:13:19 +0200

    Minimally working GUI

---
+++ src/temperature/Temperature.java
@@ -0,0 +1,9 @@
+package temperature;
+
+/**
+ * Created by Yvan on 21/06/18.
+ * Copyright (C) 2018.
+ */
+public class Temperature {
+
+}
---
+++ src/temperature/TemperatureController.java
@@ -0,0 +1,8 @@
+package temperature;
+
+/**
+ * Created by Yvan on 21/06/18.
+ * Copyright (C) 2018.
+ */
+public class TemperatureController {
+}
